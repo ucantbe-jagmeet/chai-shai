dd7c6cbc1cf44a104b725f907ea26d5a5d103c4c
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-21 17:24:42 +0530

    creating functions

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,7 +6,31 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
-async function main() {}
+async function main() {
+  async function getBalances(address) {
+    const balanceBigInt = await hre.ethers.provider.getBalance(address);
+    return hre.ethers.utils.formatEther(balanceBigInt);
+  }
+  async function consoleBalances(addresses) {
+    let counter = 0;
+    for (const address of addresses) {
+      console.log(`Address ${counter} balance:`, await getBalances(address));
+      counter++;
+    }
+  }
+  async function consoleMemos(memos) {
+    for (const memo of memos) {
+      const timestamp = memo.timestamp;
+      const name = memo.name;
+      const from = memo.address;
+      const message = memo.message;
+
+      console.log(
+        `At ${timestamp}, name ${name}, address ${from}, message ${message}`
+      );
+    }
+  }
+}
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
